1611baedc0eb24bc3cef8edc277623d791395e7c
shahir143 <Shahirsyed143@gmail.com>
2023-12-05 11:51:17 +0530

    Your user's password is at risk! use bcrypt

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,13 @@
 const User=require('../model/login');
+const bcrypt=require('bcrypt');
 
 exports.signup = async (req, res, next) => {
     try {
-        const { email } = req.body;
-        const existingUser = await User.findOne({ userEmail: email });
+        const saltRounds = 10;
+        const { email, password } = req.body;
+
+        const hash = await bcrypt.hash(password, saltRounds);
+        const existingUser = await User.findOne({ where: { userEmail: email } });
 
         if (existingUser) {
             return res.status(409).json({ success: false, message: 'User already exists' });
@@ -11,37 +15,39 @@ exports.signup = async (req, res, next) => {
 
         const user = await User.create({
             userName: req.body.name,
-            userEmail: req.body.email,
-            userPassword: req.body.password
+            userEmail: email,
+            userPassword: hash
         });
         console.log(user);
-        res.status(201).json({ success: true, message: 'User created successfully' });
+        return res.status(201).json({ success: true, message: 'User created successfully' });
     } catch (error) {
         console.error('Error in user signup:', error);
         res.status(500).json({ success: false, message: 'Error signing up user' });
     }
 };
+
 exports.login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
-        const existingUser = await User.findOne({ userEmail: email });
-        console.log(existingUser)
-        if (existingUser) {
-            if (existingUser.dataValues.userPassword === password && existingUser.dataValues.userEmail===email) {
-                return res.status(201).json({ success: true, message: 'Login successful' });
-            }else if (existingUser.dataValues.userEmail!==email){
-                return res.status(401).json({ success: false, message: 'email Wrong' });
-            }else if(existingUser.dataValues.userPassword !== password){
-                return res.status(401).json({ success: false, message: 'Password Wrong' });
-            }
-        }else{
-        return res.status(404).json({ success: false, message: 'User not found' });
-    }
+        const existingUser = await User.findOne({ where: { userEmail: email } });
+
+        if (!existingUser) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser.userPassword);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ success: false, message: 'Password wrong' });
+        }
+
+        res.status(200).json({ success: true, message: 'Login successful' });
     } catch (error) {
         console.error('Error in user login:', error);
         res.status(500).json({ success: false, message: 'Error logging in user' });
     }
-}
+};
+
 
 
 
