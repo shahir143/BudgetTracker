ba0310cf8fdba099fa0ae19e83019153fb86dd3e
shahir143 <Shahirsyed143@gmail.com>
2023-12-13 12:22:52 +0530

    db change

--- util/database.js
+++ util/database.js
@@ -1,15 +1,15 @@
-const Sequelize=require('sequelize');
+const Sequelize = require('sequelize');
 
-const schema=process.env.AWS_DB_NAME;
-const userName=process.env.AWS_DB_USERNAME;
-const userPassword=process.env.AWS_DB_PASSWORD;
+const schema = process.env.AWS_DB_NAME;
+const userName = process.env.AWS_DB_USERNAME;
+const userPassword = process.env.AWS_DB_PASSWORD;
 
-const dbDialect=process.env.AWS_DB_DIALECT;
-const db_host=process.env.AWS_DB_HOST;
+const dbDialect = process.env.AWS_DB_DIALECT;
+const db_host = process.env.AWS_DB_HOST;
 
-const sequelize=new Sequelize(schema,userName,userPassword,{
-    dialect:dbDialect,
-    host:db_host,
-})
+const sequelize = new Sequelize(schema, userName, userPassword, {
+    dialect: dbDialect,
+    host: db_host,
+});
 
 module.exports = sequelize;
