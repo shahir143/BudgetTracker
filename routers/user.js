84ead8d0428bb970647115c277f4eeff16a673cf
shahir143 <Shahirsyed143@gmail.com>
2023-12-20 14:53:11 +0530

    RANdom mistake

--- routers/user.js
+++ routers/user.js
@@ -6,6 +6,6 @@ route.get('/signup',controllerLogin.signupPage);
 route.post('/signup',controllerLogin.signup);
 route.get('/login',controllerLogin.loginPage);
 route.post('/login',controllerLogin.login);
-route.get('/',contro)
+
 
 module.exports=route;
