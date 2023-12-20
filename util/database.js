b40bacea31dd91e59dd73623de4af8343e168f49
shahir143 <Shahirsyed143@gmail.com>
2023-12-20 14:43:33 +0530

    jenkins check

--- util/database.js
+++ util/database.js
@@ -3,10 +3,9 @@ const schema=process.env.DB_NAME;
 const userName=process.env.DB_USERNAME;
 const userPassword=process.env.DB_PASSWORD;
 const db_host=process.env.DB_HOST;
-const dialect=process.env.DB_DIALECT;
 
 const sequelize=new Sequelize(schema,userName,userPassword,{
-    dialect:dialect,
+    dialect:'mysql',
     host:db_host,
 })
 
